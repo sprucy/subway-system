7f6f548b0fa8bc7eb8d24c139008fca09d4959b0
sprucy <45484048+sprucy@users.noreply.github.com>
2024-03-10 15:45:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QPixmap pixmap(":/icon/icon/splash.png");
-    QSplashScreen splash(pixmap);//显示启动画面
+    QSplashScreen splash(pixmap); //Display startup screen
     for(qint64 i=0;i<5000000;i++)
       splash.show();
     bool flag = metroGraph->readFileData(":/data/data/metroLine.txt");
     if (!flag)
     {
         QMessageBox box;
-        box.setWindowTitle(u8"错误");
+        box.setWindowTitle(u8"Error");
         box.setIcon(QMessageBox::Warning);
-        box.setText(u8"读取数据错误!\n将无法展示地铁线路！");
-        box.addButton(u8"确定", QMessageBox::AcceptRole);
+        box.setText(u8"Error reading data! \n Will not be able to display metro lines!");
+        box.addButton(u8"Confirm", QMessageBox::AcceptRole);
         if (box.exec() == QMessageBox::Accepted)
         {
             box.close();
