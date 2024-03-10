0e97081899b42870380aac9715aa1c045ec86794
sprucy <45484048+sprucy@users.noreply.github.com>
2024-03-10 15:28:10 +0100

    Update addstation.h

--- addstation.h
+++ addstation.h
@@ -21,9 +21,9 @@ class AddStation : public QDialog
     Q_OBJECT
 
 private slots:
-    //点击选择颜色按钮
+    //Click on the Select Colour button
     void on_pushButtonChooseColor_clicked();
-    //列表部件选择项改变
+    //Change List widget selections
     void on_listWidget_itemClicked(QListWidgetItem *item);
 
     void on_comboBoxConnectLine_currentIndexChanged(const QString &arg1);
@@ -31,39 +31,39 @@ private slots:
     void on_pushButtonexit_clicked();
 
 public:
-    //构造函数
+    // Constructor
     explicit AddStation(QWidget *parent = nullptr);
-    //析构函数
-    ~AddStation();
-    //设置所有部件可见
+    //Destructor
+    ~AddStation().
+    //Set all widgets visible
     void setAllVisible();
-    //设置添加线路部件可见
-    void setAddLineVisible();
-    //设置添加站点部件可见
-    void setAddStationVisible();
-    //设置添加连接部件可见
-    void setAddConnectionVisible();
-    //设置文本添加部件可见
-    void setAddByTextVisible();
-    //更新线路列表信息
-    void updateLinesListWidget();
-    //更新选择部件
-    void updateComboBox();
+    //Set the add line component visible
+    void setAddLineVisible();; //Set the add line widget visible.
+    //Set the add station part visible
+    void setAddStationVisible();; //Set Add Station widget visible.
+    //Set Add Connection Visible.
+    void setAddConnectionVisible();; //Set the Text Add widget visible.
+    //Set add text widget visible
+    void setAddByTextVisible();; //Update the line list information.
+    //Update Lines List information
+    void updateLinesListWidget();; //Update Lines List information.
+    //Update the selection widget
+    void updateComboBox();; //Update the selection widget.
 
 protected:
-    Ui::AddStation *ui;                //UI
+    Ui::AddStation *ui; //UI
 
-    QString lineName;                   //保存输入线路名
-    QColor lineColor=QColor(255,0,0);                   //保存输入线路颜色
-    QString stationName;                //保存输入站点名
-    double X;                   //保存输入站点X
-    double Y;                    //保存输入站点Y
-    QList<QString> linesNameList;       //保存选择线路名表
-    QList<QString> linesSelected;       //保存选择的线路名
-    QList<QString> stationsNameList;    //保存选择站点名表
+    QString lineName; //Store the input line name
+    QColor lineColor=QColor(255,0,0); //holds the input line colour
+    QString stationName; //Save input station name
+    double X; //Save input station X
+    double Y; //Save input station Y
+    QList<QString> linesNameList; //Save the list of selected line names.
+    QList<QString> linesSelected; //Save the selected line name.
+    QList<QString> stationsNameList; //holds the list of selected station names
 
-    //声明友元
-    friend class MainWindow;
+    //Declare the friend class MainWindow; //Declare the friend class MainWindow.
+    friend class MainWindow.
 };
 
 #endif // ADDSTATION_H
